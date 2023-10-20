5deabd464793da945e8726dd19bbc99d0a9dcab1
Chris Melvin <chris@lonestarr.xyz>
2023-10-20 11:24:00 -0400

    get utxos in node express app

--- server/index.js
+++ server/index.js
@@ -1,2 +1,22 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const port = 7000;
+
+app.listen(port, () => {
+    console.log(`server has started on port`, port)
+})
+
+//middleware
+app.use(cors());
+app.use(express.json()); //req.body
+
+app.get("/utxos", async(req,res) => {
+    try {
+        const response = await fetch("http://127.0.0.1:8000/utxo/list");
+        const jsonData = await response.json();
+        res.json(jsonData);
+    } catch (error) {
+        console.log(error.message);
+    }
+});
